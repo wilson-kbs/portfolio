7cfb0aae978f8c61e35ecbbb94d4b2d8737d7578
wilson-kbs <kabalisa.wilson@gmail.com>
2021-12-14 17:52:33 +0100

    refactor(api): addition of a global prefix on /api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   await app.listen(process.env.KBSV_PORTFOLIO_SERVER_PORT || 9000);
 }
 bootstrap();
